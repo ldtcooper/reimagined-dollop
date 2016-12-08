e6c635601ae7fb8270fcc01b50065639c9515f41
Logan Cooper <lcooper@ucsc.edu>
2016-12-07 17:04:08 -0800

    Encryption function fully encrypts messages

--- transposition cipher.py
+++ transposition cipher.py
@@ -49,12 +49,33 @@ def encryption(plaintext, grid, key):
     """Encrypts the plaintext using a grid of a size determined by the grid_gen function"""
     # counter for which sub-list letters will be appended to
     lst =0
+    # loop to write plaintext to grid
     for letter in plaintext:
         # puts letters into grid
         grid[lst].append(letter)
         # moves to the next row after all the columns are filled in
         if len(grid[lst]) == key:
             lst += 1
-    print(grid)
+    # empty list for ciphertext to be put into
+    cipher_list = []
+    # counter for which column the function will draw letters from
+    take = 0
+    # loop to read plain text from grid as ciphertext
+    while len(cipher_list) < len(plaintext):
+        # iterates through the columns of the grid
+        for elem in grid:
+            try:
+                cipher_list.append(elem[take])
+            # if the function finds a list with too few indices, it will know that it has reached the end of the message
+            except IndexError:
+                print(''.join(cipher_list))
+                break
+        take += 1
 
 
+
+
+
+
+G = grid_gen("Hello there world", 3)
+encryption("Hello there world", G, 3)
